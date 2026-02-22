0004b7cd3c77aee04455302c694db23bb4334c60
himanshi-2005 <himanshihimanshi631@gmail.com>
2026-02-21 18:17:52 -0700

    Remove comments from RootLayout component

--- music-app - Copy/app/_layout.tsx
+++ music-app - Copy/app/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* (tabs) group handles the tab bar */}
+      
       <Stack.Screen name="(tabs)" />
-      {/* free-trial is a stack screen pushed on top of tabs */}
+      
       <Stack.Screen name="free-trial" />
     </Stack>
   );
